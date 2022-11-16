9915711bdf3368c59c1fb2c54a82ca1990aab0cc
Bao Bao <nguyenbao.nix@gmail.com>
2022-11-16 17:10:33 +0700

    config cors 4

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const corsOptions = {
+	origin: "*",
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+};
 const path = require("path");
 const bodyParser = require("body-parser");
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors({ origin: "*" }));
+app.use(cors(corsOptions));
 app.use("/public", express.static(path.join(__dirname, "./public")));
 // Routes middle
 const route = require("./routes/web");
