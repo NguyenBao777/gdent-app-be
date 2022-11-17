c000bec79729f9e00eea9fa729b2bb9fc70985ac
Bao Bao <nguyenbao.nix@gmail.com>
2022-11-17 16:00:34 +0700

    fix app=>index

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use("/public", express.static(path.join(__dirname, "./public")));
 app.get("/", (req, res) => {
-	return res.status(200).send({ message: "server gdent-app" });
+	res.status(200).send({ message: "server gdent-app" });
 });
 // Routes middle
 const route = require("./routes/web");
